2ad1fb8041289eb410eb359a6f5d0cb0aa7b521f
M.Aqib <m.aqibayub@gmail.com>
2021-07-10 12:40:10 +0500

    implemented firebase utils, including user Auth datatbase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,33 @@ const firebaseConfig = {
   appId: "1:278734800786:web:498502a49fec608f0ae34b",
   measurementId: "G-L6QVBXEE8F",
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return userRef;
+};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
